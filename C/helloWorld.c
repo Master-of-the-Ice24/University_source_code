6bf2b9555ff15b5129587be8738b4553d35690a3
Manuel Monesi <138038977+Master-of-the-Ice24@users.noreply.github.com>
2025-09-29 16:12:54 +0200

    Add files via upload

---
+++ C/helloWorld.c
@@ -0,0 +1,16 @@
+/*
+
+la funzione printf prende come argomento una stringa di testo e la stampa a schermo
+
+*/
+
+
+
+#include <stdio.h>
+
+
+int main() {
+	printf("HelloWorld!\n");
+
+	return 0;
+}
