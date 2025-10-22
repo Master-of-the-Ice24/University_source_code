e6ca07aade09f59534f9fc621c572c91f0d7fee8
Manuel Monesi <master.ice24@proton.me>
2025-10-22 12:58:54 +0200

    updates

--- C/esercizi/my_exercise_1/main.c
+++ C/esercizi/my_exercise_1/main.c
@@ -1,39 +1,63 @@
 #include <ncurses.h>
-#include <menu.h>
+//#include <menu.h>
+//#include <stdlib.h>
+
 #include "./src/basicFuncs.h"
 
 #define ESCAPE 27
 #define ENTER 10
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-char *choises[] = {
-					"Choise 1",
-					"Choise 2",
-					"Choise 3"
+char *menuItems[] = {
+					"Choice 1",
+					"Choice 2",
+					"Choice 3"
 				};
 
+int numItems = sizeof(menuItems) / sizeof(menuItems[0]);
 
+
+void displayMenu(int highlight) {
+	clear();
+	for (int i=0; i<numItems; i++) {
+		if (i == highlight) 
+			attron(A_REVERSE);
+		mvprintw(i+1, 1, "%s", menuItems[i]);
+		if (i == highlight)
+			attroff(A_REVERSE);
+	}
+	refresh();
+}
+
+/*
 
 int main() {
-	int key;
-	
-	do {
-		initscr();
-		//keypad(stdscr, FALSE);
-		noecho();
+	int highlight = 0;
+	int choice;
 
-		key = getch();		//getch() displays a value but underneath gets an integer number
-	
-		printw("hello enter a string: ");
-		/*
-		char *myString;
-		getstr(myString);
-		//myString = testingText();
-		addstr(myString);
-		*/
-		refresh();
-	} while (key != ENTER);
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
 
-	endwin();
+	while(TRUE) {
+		displayMenu(highlight);
+		choice = getch();
+
+		switch(choice) {
+			case KEY_UP:
+				highlight = (highlight-1 + numItems) % numItems;
+				break;
+			case KEY_DOWN:
+				highlight = (highlight+1 + numItems) % numItems;
+				break;	
+		}
+
+		mvprintw(numItems +2, 1, "You selected %s", menuItems[highlight]);
+	}
+
+
 	return 0;
 }
+
+*/
+
