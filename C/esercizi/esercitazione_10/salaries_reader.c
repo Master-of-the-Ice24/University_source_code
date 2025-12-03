7f0850b241abd2e9d0da15854dfbb21d1178621d
Manuel Monesi <master.ice24@proton.me>
2025-12-03 20:39:30 +0100

    esercitazione_10 updated

--- C/esercizi/esercitazione_10/salaries_reader.c
+++ C/esercizi/esercitazione_10/salaries_reader.c
@@ -12,19 +12,25 @@ int main() {
         exit(1);
     }
 
+    /*
     Employee germano;
 
     while(fread(&germano, sizeof(Employee), 1, document)) {
-        printf("data: %s ", germano.name);
+        //printf("data: %s ", germano.name);
+    }
+    */
+
+    Employee vector[5];
+
+    for (int i=0; i<5; i++) {
+        fread(&vector[i], sizeof(Employee), 1 ,document);
+        printf("\n%s %0.2f", vector[i].name, vector[i].salary);
     }
 
     if (fclose(document) != 0) {
         printf("CLOSING ERROR shutting down...");
         exit(2);
     }
-
-
-
-
+    
     return 0;
 }
